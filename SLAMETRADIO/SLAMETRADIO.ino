e135f1af2ac2f297ea4ebf93c321cb8fe13f13b7
felangga <felangga@gmail.com>
2020-04-10 02:39:11 +0700

    Update SLAMETRADIO.ino

--- SLAMETRADIO/SLAMETRADIO.ino
+++ SLAMETRADIO/SLAMETRADIO.ino
@@ -20,10 +20,6 @@ TFT_eSPI    tft = TFT_eSPI();
 #define SW_BAND_TYPE 2
 #define LW_BAND_TYPE 3
 
-// OLED Diaplay constants
-#define I2C_ADDRESS 0x3C
-#define RST_PIN -1 // Define proper RST_PIN if required.
-
 #define RESET_PIN 12
 
 // Enconder PINs
@@ -45,7 +41,7 @@ TFT_eSPI    tft = TFT_eSPI();
 #define MIN_ELAPSED_TIME 100
 #define MIN_ELAPSED_RSSI_TIME 150
 
-#define DEFAULT_VOLUME 100 // change it for your favorite sound volume
+#define DEFAULT_VOLUME 50 // change it for your favorite sound volume
 
 #define FM 0
 #define LSB 1
@@ -91,6 +87,11 @@ uint8_t bwIdxAM = 1;
 const char *bandwitdthAM[] = {"6", "4", "3", "2", "1", "1.8", "2.5"};
 unsigned int buttonstate;
 
+const char *AMMode[] = {"AM Mode", "LSB", "USB"};
+uint8_t idxAMMode = 0;
+
+char buffer[64];
+
 typedef struct
 {
   const char *bandName;
@@ -104,9 +105,9 @@ typedef struct
 } Band;
 
 Band band[] = {
-  {"FM  ", FM_BAND_TYPE, 8400, 10800, 10390, 10, 100.0},
-  {"LW  ", LW_BAND_TYPE, 100, 510, 300, 1, 1.0},
-  {"AM  ", MW_BAND_TYPE, 520, 1720, 810, 10, 1.0},
+  {"FM", FM_BAND_TYPE, 8400, 10800, 9420, 10, 100.0},
+  {"LW", LW_BAND_TYPE, 100, 510, 300, 1, 1.0},
+  {"AM", MW_BAND_TYPE, 520, 1720, 810, 10, 1.0},
   {"160m", SW_BAND_TYPE, 1800, 3500, 1900, 1, 1000.0}, // 160 meters
   {"80m ", SW_BAND_TYPE, 3500, 4500, 3700, 1, 1000.0}, // 80 meters
   {"60m ", SW_BAND_TYPE, 4500, 5500, 4850, 5, 1000.0},
@@ -164,12 +165,9 @@ boolean fast = false;
 Rotary encoder = Rotary(ENCODER_PIN_A, ENCODER_PIN_B);
 SI4735 si4735;
 
-// Use Rotary.h and  Rotary.cpp implementation to process encoder via interrupt
 void IRAM_ATTR rotaryEncoder()
-{ // rotary encoder events
-
+{
   uint8_t encoderStatus = encoder.process();
-
   if (encoderStatus)
   {
     if (encoderStatus == DIR_CW)
@@ -228,6 +226,7 @@ void setup()
   attachInterrupt(ENCODER_PIN_B, rotaryEncoder, CHANGE);
 
   useBand();
+
   delay(200);
 
 
@@ -244,140 +243,37 @@ void setup()
   btnAGC = {30, 50, 60, 30, "AGC"};
   btnPrevBand = {110, 50, 80, 30, "Prev Band"};
   btnNextBand = {210, 50, 80, 30, "Next Band"};
-  btnMode = {30, 90, 60, 30, "AM Mode"};
+  btnMode = {30, 90, 60, 30, (char*) AMMode[idxAMMode]};
 
   daftarTombol();
+
+
+  getStatus();
 }
 
-
-//void gantiBand(int band) {
-//  switch (band) {
-//    // FM
-//    case 1 :
-//      currentBottomFreq = 8800;
-//      currentTopFreq = 10800;
-//      currentPembagi = 100.0;
-//      currentFrequency = currentBottomFreq;
-//      currentSatuan  = "MHz";
-//      currentDecimal = 2;
-//      currentMode = FM;
-//      si4735.setTuneFrequencyAntennaCapacitor(0);
-//      si4735.setFM(currentBottomFreq, currentTopFreq, 9800, 10);
-//      bfoOn = ssbLoaded = false;
-//      break;
-//    // LW
-//    case 2 :
-//      currentBottomFreq = 100;
-//      currentTopFreq = 510;
-//      currentPembagi = 1;
-//      currentSatuan  = "KHz";
-//      currentDecimal = 0;
-//      currentFrequency = 300;
-//      setAM(currentBottomFreq, currentTopFreq, currentFrequency, 1);
-//      break;
-//    // MW
-//    case 3 :
-//      currentBottomFreq = 520;
-//      currentTopFreq = 1720;
-//      currentPembagi = 1;
-//      currentDecimal = 0;
-//      currentSatuan  = "KHz";
-//      currentFrequency = currentBottomFreq;
-//      setAM(currentBottomFreq, currentTopFreq, currentFrequency, 10);
-//      break;
-//    // SW
-//    case 4 :
-//      currentBottomFreq = 1800;
-//      currentTopFreq = 28000;
-//      currentPembagi = 1000.0;
-//      currentDecimal = 3;
-//      currentSatuan  = "KHz";
-//      currentFrequency = currentBottomFreq;
-//      setAM(currentBottomFreq, currentTopFreq, currentFrequency, 1);
-//      break;
-//  }
-//
-//  geserSebelum = ((currentTopFreq - currentFrequency) / currentPembagi) * 30;
-//
-//}
-
-
-/*
-    Show some basic information on display
-*/
-//void showStatus()
-//{
-//
-//  showFrequency();
-//
-//  //  display.setCursor(13, 1);
-//  //  display.print("      ");
-//  //  display.setCursor(13, 1);
-//  //  display.print("St: ");
-//  //  display.print(currentStep);
-//  //
-//  //  display.setCursor(0, 3);
-//  //  display.print("           ");
-//  ////  display.setCursor(0, 3);
-//  //
-//  //  if (currentMode == LSB || currentMode == USB)
-//  //  {
-//  //    display.print("BW:");
-//  //    display.print(String(bandwitdthSSB[bwIdxSSB]));
-//  //    display.print("KHz");
-//  //    showBFO();
-//  //  }
-//  //  else if (currentMode == AM)
-//  //  {
-//  //    display.print("BW:");
-//  //    display.print(String(bandwitdthAM[bwIdxAM]));
-//  //    display.print("KHz");
-//  //  }
-//  //
-//  //  // Show AGC Information
-//  //  si4735.getAutomaticGainControl();
-//  //  display.setCursor(0, 1);
-//  //  display.print((si4735.isAgcEnabled()) ? "AGC ON " : "AGC OFF");
-//
-//  showRSSI();
-//  showVolume();
-//}
-
-/* *******************************
-   Shows RSSI status
-*/
 void showRSSI()
 {
+  Serial.println(rssi);
+  tft.drawRoundRect(0, 0, 117, 31, 4, 0xFFFF);
+  //  // draw signal bar
+  tft.fillRect(((rssi * 2.8)) + 1, 2, 114 - ((rssi * 2.8)), 14, 0x0000);
+  tft.fillRect(2, 2, rssi * 2.8 , 15, 0x0A00);
+  //
+  tft.fillRect(((snr * 2.8)) + 1, 15, 114 - ((snr * 2.8)), 15, 0x0000);
+  tft.fillRect(2, 15, snr * 2.8 , 15, 0xAA00);
 
-  // draw signal bar
-  tft.fillRect(((rssi * 8)), 0, screenWidth, 15, 0x0000);
-  tft.fillRect(0, 0, rssi * 8 , 15, 0x0A00);
-
-  tft.fillRect(((snr * 4)), 15, screenWidth, 15, 0x0000);
-  tft.fillRect(0, 15, snr * 8 , 15, 0xAA00);
-
-  tft.setCursor(2, 4);
+  tft.setCursor(4, 5);
   tft.setTextColor(0xFFFF);
   tft.setTextWrap(false);
   tft.setTextSize(1);
 
   tft.print("SGNL");
 
-  tft.setCursor(2, 19);
+  tft.setCursor(4, 19);
   tft.print("SNR");
 
 }
 
-/*
-   Shows the volume level on LCD
-*/
-//void showVolume()
-//{
-//  //  display.setCursor(10, 3);
-//  //  display.print("  ");
-//  //  display.setCursor(10, 3);
-//  //  display.print(si4735.getCurrentVolume());
-//}
 
 /*
    Shows the BFO current status.
@@ -454,33 +350,33 @@ void bandDown()
    This function loads the contents of the ssb_patch_content array into the CI (Si4735) and starts the radio on
    SSB mode.
 */
-//void loadSSB()
-//{
-//
-//
-//  si4735.reset();
-//  si4735.queryLibraryId(); // Is it really necessary here?  Just powerDown() maigh work!
-//  si4735.patchPowerUp();
-//  delay(50);
-//  // si4735.setI2CFastMode(); // Recommended
-//  si4735.setI2CFastModeCustom(500000); // It is a test and may crash.
-//  si4735.downloadPatch(ssb_patch_content, size_content);
-//  si4735.setI2CStandardMode(); // goes back to default (100KHz)
-//
-//
-//  // delay(50);
-//  // Parameters
-//  // AUDIOBW - SSB Audio bandwidth; 0 = 1.2KHz (default); 1=2.2KHz; 2=3KHz; 3=4KHz; 4=500Hz; 5=1KHz;
-//  // SBCUTFLT SSB - side band cutoff filter for band passand low pass filter ( 0 or 1)
-//  // AVC_DIVIDER  - set 0 for SSB mode; set 3 for SYNC mode.
-//  // AVCEN - SSB Automatic Volume Control (AVC) enable; 0=disable; 1=enable (default).
-//  // SMUTESEL - SSB Soft-mute Based on RSSI or SNR (0 or 1).
-//  // DSP_AFCDIS - DSP AFC Disable or enable; 0=SYNC MODE, AFC enable; 1=SSB MODE, AFC disable.
-//  si4735.setSSBConfig(bwIdxSSB, 1, 0, 0, 0, 1);
-//  delay(25);
-//  ssbLoaded = true;
-//
-//}
+void loadSSB()
+{
+
+
+  si4735.reset();
+  si4735.queryLibraryId(); // Is it really necessary here?  Just powerDown() maigh work!
+  si4735.patchPowerUp();
+  delay(50);
+  // si4735.setI2CFastMode(); // Recommended
+  si4735.setI2CFastModeCustom(500000); // It is a test and may crash.
+  si4735.downloadPatch(ssb_patch_content, size_content);
+  si4735.setI2CStandardMode(); // goes back to default (100KHz)
+
+
+  // delay(50);
+  // Parameters
+  // AUDIOBW - SSB Audio bandwidth; 0 = 1.2KHz (default); 1=2.2KHz; 2=3KHz; 3=4KHz; 4=500Hz; 5=1KHz;
+  // SBCUTFLT SSB - side band cutoff filter for band passand low pass filter ( 0 or 1)
+  // AVC_DIVIDER  - set 0 for SSB mode; set 3 for SYNC mode.
+  // AVCEN - SSB Automatic Volume Control (AVC) enable; 0=disable; 1=enable (default).
+  // SMUTESEL - SSB Soft-mute Based on RSSI or SNR (0 or 1).
+  // DSP_AFCDIS - DSP AFC Disable or enable; 0=SYNC MODE, AFC enable; 1=SSB MODE, AFC disable.
+  si4735.setSSBConfig(bwIdxSSB, 1, 0, 0, 0, 1);
+  delay(25);
+  ssbLoaded = true;
+
+}
 
 void useBand()
 {
@@ -533,12 +429,14 @@ void drawFreq() {
     if (band[bandIdx].bandType == FM_BAND_TYPE) currentDecimal = 2;
     if (band[bandIdx].bandType == MW_BAND_TYPE || band[bandIdx].bandType == LW_BAND_TYPE) currentDecimal = 0;
 
-    String convert = " " + String(currentFrequency / band[bandIdx].currentPembagi, currentDecimal) + " " + ((currentMode == FM) ? "MHz" : "KHz") ;
+//    String convert = " " + String(currentFrequency / band[bandIdx].currentPembagi, currentDecimal) + " " + ((currentMode == FM) ? "MHz" : "KHz") ;
+    sprintf(freq, "%6s %s", String(currentFrequency / band[bandIdx].currentPembagi, currentDecimal), ((currentMode == FM) ? "MHz" : "KHz"));
 
-    convert.toCharArray(freq, 20);
+
+//    convert.toCharArray(freq, 20);
 
     tft.setTextSize(3);
-    tft.setTextColor(0xFFFF);
+    tft.setTextColor(0xFFFF, 0x0000);
     tft.drawRightString(freq, screenWidth - 2, 4, 1);
     previousFrequency = currentFrequency;
   }
@@ -661,9 +559,24 @@ bool tombolPressed(int x, int y, Tombol button) {
 }
 
 void getStatus() {
-  si4735.getAutomaticGainControl();
+  si4735.getStatus();
 
-  gambarTombol(btnAGC, si4735.isAgcEnabled());
+
+  String bufferStereo = "       ";
+  if ( currentMode == FM ) {
+    if  (si4735.getCurrentPilot()) bufferStereo = "STEREO"; else bufferStereo = " MONO ";
+  }
+
+  si4735.getAutomaticGainControl();
+  disableAgc = !si4735.isAgcEnabled();
+  sprintf(buffer, "%s %s %s %s", bufferStereo, (si4735.isAgcEnabled()) ? "AGC" : "  ", band[bandIdx].bandName, (fast) ? "FAST" : "SLOW");
+
+  tft.setTextSize(1);
+  tft.setTextColor(0xFFFF, 0x0000);
+  tft.drawRightString(buffer, screenWidth - 3, 33, 1);
+
+  gambarTombol(btnAGC, !disableAgc);
+
 }
 
 void daftarTombol() {
@@ -715,6 +628,41 @@ void loop()
 
     if (tombolPressed(x, y, btnMode) && (currentMode != FM)) {
       gambarTombol(btnMode, true);
+      idxAMMode ++;
+      if (idxAMMode > 2) idxAMMode = 0;
+      btnMode = {30, 90, 60, 30, (char*) AMMode[idxAMMode]};
+
+      if (idxAMMode == 1) {
+        // LSB
+        if (currentMode != FM ) {
+          if (!ssbLoaded) {
+            loadSSB();
+          }
+          currentMode = LSB;
+          band[bandIdx].currentFreq = currentFrequency;
+          band[bandIdx].currentStep = currentStep;
+          useBand();
+        }
+      } else if (idxAMMode == 2) {
+        // USB
+        if (currentMode != FM ) {
+          if (!ssbLoaded) {
+            loadSSB();
+          }
+          currentMode = USB;
+          band[bandIdx].currentFreq = currentFrequency;
+          band[bandIdx].currentStep = currentStep;
+          useBand();
+        }
+      } else if (idxAMMode == 0) {
+        band[bandIdx].currentFreq = currentFrequency;
+        band[bandIdx].currentStep = currentStep;
+        ssbLoaded = false;
+        bfoOn = false;
+        currentMode = AM;
+        //        bandIdx = 2;
+        useBand();
+      }
 
       delay(MIN_ELAPSED_TIME);
     }
@@ -726,23 +674,6 @@ void loop()
   }
 
   if (millis() - waktuTerakhir > 700) fast = false;
-
-  // Show fast & slow tuning text
-  if (fast) {
-    tft.setTextSize(1);
-    tft.setTextColor(0xFFFF, 0x0000);
-    tft.drawRightString("FAST", screenWidth - 3, 33, 1);
-  } else {
-    tft.setTextSize(1);
-    tft.setTextColor(0xFFFF, 0x0000);
-    tft.drawRightString("SLOW", screenWidth - 3 , 33, 1);
-  }
-
-
-  tft.setTextSize(1);
-  tft.setTextColor(0xFFFF, 0x0000);
-  tft.drawRightString(band[bandIdx].bandName, screenWidth - 40, 33, 1);
-
 
 
   if (encoderCount != 0)
@@ -794,13 +725,15 @@ void loop()
     if (rssi != aux)
     {
       rssi = aux;
-      previousFrequency = 0;
       showRSSI();
+      getStatus();
+      previousFrequency = 0;
     }
     elapsedRSSI = millis();
   }
 
   drawFreq();
+
 
 
   delay(10);
